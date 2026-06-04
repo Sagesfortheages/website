a752525abecab382d78d89622b6b0368e2879dfb
Sagesfortheages <sagesfortheages@gmail.com>
2026-06-04 12:10:22 -0400

    Change image storage from 'images' to 'public_images'

--- texts/texts.js
+++ texts/texts.js
@@ -2,8 +2,8 @@ import { supabaseClient } from '../supabase/supabaseClient.js';
 
 export async function loadTextImage(filename) {
     const { data, error } = await supabaseClient.storage
-        .from('images')
-        .createSignedUrl(`texts/${filename}`, 60); // valid for 5 minutes
+        .from('public_images')
+        .getPublicUrl(`texts/${filename}`);
 
     return data
 }
